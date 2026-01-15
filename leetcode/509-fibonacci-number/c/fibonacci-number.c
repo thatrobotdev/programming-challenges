18d06d45ad063cac2f90e3db6ab9d6942cdc52b4
James Kerrane <hi@jameskerrane.com>
2026-01-15 15:09:54 -0500

    Leetcode 509: Fibonacci Number improvements
    Python + C:
    * Clarifies comments
    * Replaces double comparison (n == 0, n == 1) with single comparison (n < 2)
    
    Python:
    * Implement polynomial algorithm and allow switching between the two at runtime
    * Use formatted print string
    
    C:
    * Implement polynomial algorithm with sliding window

--- leetcode/509-fibonacci-number/c/fibonacci-number.c
+++ leetcode/509-fibonacci-number/c/fibonacci-number.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 
+// Choose max number of fib sequence generated
 #define MAX 30
-#define fib fib_recursive
 
-// Exponential (recursive) algorithm
+// Choose fib implementation
+#define fib fib_polynomial_sliding
+
+/*
+    Exponential (recursive algorithm)
+    Complexity:
+        Time: O(φ^n)
+        Space: O(n)
+*/
 int fib_recursive(int n) {
-    if(n == 0) return 0;
-    if(n == 1) return 1;
+    if(n < 2) return n;
     return fib_recursive(n - 1) + fib_recursive(n - 2);
 }
 
-// Polynomial algorithm
-int fib_polynomial(int n) {
-    int fibNums[n + 2];
+/*
+    Unoptomized polynomial algorithm using array
+    Complexity:
+        Time: O(n)
+        Space: O(n)
+*/
+int fib_polynomial_array(int n) {
+    if(n < 2) return n;
+
+    int fibNums[n];
 
     fibNums[0] = 0;
     fibNums[1] = 1;
@@ -22,6 +36,27 @@ int fib_polynomial(int n) {
     }
 
     return fibNums[n];
+}
+
+/*
+    Optimized polynomial algorithm using sliding window
+    Complexity:
+        Time: O(n)
+        Space: O(1)
+*/
+int fib_polynomial_sliding(int n) {
+    if(n < 2) return n;
+
+    int prev = 0;
+    int curr = 1;
+
+    for(int i = 2; i <= n; i++) {
+        int next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
 }
 
 int main() {
--- leetcode/509-fibonacci-number/python3/solution.py
+++ leetcode/509-fibonacci-number/python3/solution.py
@@ -1,14 +1,34 @@
+# Choose max number of fib sequence generated
 MAX = 30
 
 class Solution:
     # Exponential (recursive) algorithm
-    def fib(self, n: int) -> int:
-        if n == 0: return 0
-        if n == 1: return 1
-        return self.fib(n - 1) + self.fib(n - 2)
+    # Complexity:
+    #   Time: O(φ^n)
+    #   Space: O(n)
+    def fib_recursive(self, n: int) -> int:
+        if n < 2: return n
+        return self.fib_recursive(n - 1) + self.fib_recursive(n - 2)
+    
+    # Polynomial algorithm
+    # Complexity:
+    #   Time: O(n)
+    #   Space: O(n)
+    def fib_polynomial_array(self, n: int) -> int:
+        if n < 2: return n;
+    
+        fibNums = [0, 1]
+
+        for i in range(2, n + 1):
+            fibNums.append(fibNums[i - 1] + fibNums[i - 2]);
+
+        return fibNums[n];
 
 if __name__ == '__main__':
     s = Solution()
+
+    # Choose fib implementation here
+    fib = s.fib_polynomial_array
     
     for i in range(MAX + 1):
-        print("n = " + str(i) + ": " + str(s.fib(i)))
+        print(f"n = {i}: {fib(i)}")
